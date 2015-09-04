65ab44301d976730a67ea43d174915f1ba3883d5
kingoffighter97 <bao050297@gmail.com>
2015-09-04 11:50:30 -0400

    viewport

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $( document ).ready(function() {
+
+	// Make the images has same width and height
     var width = $('.member').width();
 	$('.member').css({'height':width+'px'});
 	$(window).resize(function() {
 		var width = $('.member').width();
 		$('.member').css({'height':width+'px'});
 	});
+
 });
